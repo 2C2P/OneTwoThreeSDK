8e9cf6518df4d7906f24b631cee1efe63dc668fc
Orawan Manasombun <orawan.man@2c2p.com>
2022-04-19 13:47:12 +0700

    Updated artifact name to correspond Xcode 13.3

--- Package.swift
+++ Package.swift
@@ -12,14 +12,14 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "OneTwoThreeSDK",
-            targets: ["OneTwoThreeSDKFramework", "OpenSSL"]),
+            targets: ["OneTwoThreeSDK", "OpenSSL"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
 
         .binaryTarget(
-            name: "OneTwoThreeSDKFramework",
+            name: "OneTwoThreeSDK",
             path: "./Sources/OneTwoThreeSDK.xcframework"),
         .binaryTarget(
             name: "OpenSSL",
